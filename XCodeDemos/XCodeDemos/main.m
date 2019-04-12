b9e02aedc1b403157e7659fb98d9f059ec327108
Iann Dang <iann1978@163.com>
2019-04-12 10:24:24 +0800

    + Add Project XCodeDemos.

---
+++ XCodeDemos/XCodeDemos/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XCodeDemos
+//
+//  Created by iann on 2019/4/12.
+//  Copyright © 2019 iann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
