4554ba9dbee79f796257ec219b42f7820207b9b6
Iann Dang <iann1978@163.com>
2019-04-28 16:49:14 +0800

    + Add unity's header to RenderPlugin.

--- UnityPlugin/RenderPlugin/RenderPlugin/RenderPlugin.cpp
+++ UnityPlugin/RenderPlugin/RenderPlugin/RenderPlugin.cpp
@@ -1,2 +1,16 @@
+#include "Unity/IUnityGraphics.h"
 
-extern "C" int GetVersion() { return 1; }
+extern "C" int GetVersion() { return 5; }
+
+
+extern "C" void    UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* unityInterfaces)
+{
+    int a = 0;
+    int b = 0;
+}
+
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
+{
+    int a = 0;
+    int b = 0;
+}
