86da54adba1beb747aa486a09048120320062c10
IannDang <iann1978@163.com>
2019-05-23 18:22:43 +0800

    - Modify screen to 1920x1080.

--- VSDemos/LuciUI/LuciUI.cpp
+++ VSDemos/LuciUI/LuciUI.cpp
@@ -38,10 +38,10 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	Screen::width = 1024;
-	Screen::height = 768;
+	Screen::width = 1920;
+	Screen::height = 1080;
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow(1024, 768, "Tutorial 05 - Textured Cube", NULL, NULL);
+	window = glfwCreateWindow(Screen::width, Screen::height, "Tutorial 05 - Textured Cube", NULL, NULL);
 	if (window == NULL) {
 		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
 		glfwTerminate();
