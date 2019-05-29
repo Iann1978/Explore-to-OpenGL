b229f973cee029ffb9fddae27a460094ad12aa3b
IannDang <iann1978@163.com>
2019-05-29 09:20:15 +0800

    + Add engine class Engine to OpengGLByNDK.

---
+++ AndroidDemos/OpenGLByNDK/app/src/main/cpp/engine/Engine.h
@@ -0,0 +1,29 @@
+//
+// Created by Iann on 2019/5/28.
+//
+
+
+
+#ifndef OPENGLBYNDK_ENGINE_H
+#define OPENGLBYNDK_ENGINE_H
+
+#include <EGL/egl.h>
+#include <GLES3/gl32.h>
+
+#define  LOG_TAG    "libgl2jni"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+void printGLString(const char *name, GLenum s);
+void checkGlError(const char* op);
+class Engine
+{
+public:
+    Engine();
+
+    void init(int w, int h);
+
+    void RenderOneFrame();
+};
+
+
+#endif //OPENGLBYNDK_ENGINE_H
